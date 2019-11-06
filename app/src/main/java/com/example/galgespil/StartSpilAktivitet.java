f24a921109bf78e597b56f9b617f418e993d9791
Maria K <maria.kajgaard@gmail.com>
2019-11-06 18:34:37 +0100

    Implementeret alle bogstaver i tastaturet

--- app/src/main/java/com/example/galgespil/StartSpilAktivitet.java
+++ app/src/main/java/com/example/galgespil/StartSpilAktivitet.java
@@ -15,7 +15,8 @@ import android.widget.TextView;
 public class StartSpilAktivitet extends AppCompatActivity implements View.OnClickListener {
     static Logik logik = new Logik();
     StopUr stopUr = new StopUr();
-    Button a, b, c, d, afslutKnap;
+    Button a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p , q, r, s, t, u, ve, w, x, y, z, ae, oe, aa;
+    Button afslutKnap;
     TextView ukendtOrd, liv, tid;
     ImageView hangman, hjerte;
     Runnable opdaterTid;
@@ -26,17 +27,93 @@ public class StartSpilAktivitet extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_spil_aktivitet);
 
-        a = findViewById(R.id.buttonQ);
+        a = findViewById(R.id.buttonA);
         a.setOnClickListener(this);
 
-        b = findViewById(R.id.buttonW);
+        b = findViewById(R.id.buttonB);
         b.setOnClickListener(this);
 
-        c = findViewById(R.id.buttonE);
+        c = findViewById(R.id.buttonC);
         c.setOnClickListener(this);
 
-        d = findViewById(R.id.buttonR);
+        d = findViewById(R.id.buttonD);
         d.setOnClickListener(this);
+
+        e = findViewById(R.id.buttonE);
+        e.setOnClickListener(this);
+
+        f = findViewById(R.id.buttonF);
+        f.setOnClickListener(this);
+
+        g = findViewById(R.id.buttonG);
+        g.setOnClickListener(this);
+
+        h = findViewById(R.id.buttonH);
+        h.setOnClickListener(this);
+
+        i = findViewById(R.id.buttonI);
+        i.setOnClickListener(this);
+
+        j = findViewById(R.id.buttonJ);
+        j.setOnClickListener(this);
+
+        k = findViewById(R.id.buttonK);
+        k.setOnClickListener(this);
+
+        l = findViewById(R.id.buttonL);
+        l.setOnClickListener(this);
+
+        m = findViewById(R.id.buttonM);
+        m.setOnClickListener(this);
+
+        n = findViewById(R.id.buttonN);
+        n.setOnClickListener(this);
+
+        o = findViewById(R.id.buttonO);
+        o.setOnClickListener(this);
+
+        p = findViewById(R.id.buttonP);
+        p.setOnClickListener(this);
+
+        q = findViewById(R.id.buttonQ);
+        q.setOnClickListener(this);
+
+        r = findViewById(R.id.buttonR);
+        r.setOnClickListener(this);
+
+        s = findViewById(R.id.buttonS);
+        s.setOnClickListener(this);
+
+        t = findViewById(R.id.buttonT);
+        t.setOnClickListener(this);
+
+        u = findViewById(R.id.buttonU);
+        u.setOnClickListener(this);
+
+        ve = findViewById(R.id.buttonV);
+        ve.setOnClickListener(this);
+
+        w = findViewById(R.id.buttonW);
+        w.setOnClickListener(this);
+
+        x = findViewById(R.id.buttonX);
+        x.setOnClickListener(this);
+
+        y = findViewById(R.id.buttonY);
+        y.setOnClickListener(this);
+
+        z = findViewById(R.id.buttonZ);
+        z.setOnClickListener(this);
+
+        ae = findViewById(R.id.buttonAE);
+        ae.setOnClickListener(this);
+
+        oe = findViewById(R.id.buttonOE);
+        oe.setOnClickListener(this);
+
+        aa = findViewById(R.id.buttonAA);
+        aa.setOnClickListener(this);
+
 
         afslutKnap = findViewById(R.id.afslutKnap);
         afslutKnap.setOnClickListener(this);
@@ -68,6 +145,7 @@ public class StartSpilAktivitet extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
+
         if(v == a){
             String bogstav = a.getText().toString();
             logik.gætBogstav(bogstav);
@@ -87,6 +165,128 @@ public class StartSpilAktivitet extends AppCompatActivity implements View.OnClic
             String bogstav = d.getText().toString();
             logik.gætBogstav(bogstav);
             d.setEnabled(false);
+
+        }else if(v == e){
+            String bogstav = e.getText().toString();
+            logik.gætBogstav(bogstav);
+            e.setEnabled(false);
+
+        }else if(v == f){
+            String bogstav = f.getText().toString();
+            logik.gætBogstav(bogstav);
+            f.setEnabled(false);
+
+        }else if(v == g){
+            String bogstav = g.getText().toString();
+            logik.gætBogstav(bogstav);
+            g.setEnabled(false);
+
+        }else if(v == h){
+            String bogstav = h.getText().toString();
+            logik.gætBogstav(bogstav);
+            h.setEnabled(false);
+
+        }else if(v == i){
+            String bogstav = i.getText().toString();
+            logik.gætBogstav(bogstav);
+            i.setEnabled(false);
+
+        }else if(v == j){
+            String bogstav = j.getText().toString();
+            logik.gætBogstav(bogstav);
+            j.setEnabled(false);
+
+        }else if(v == k){
+            String bogstav = k.getText().toString();
+            logik.gætBogstav(bogstav);
+            k.setEnabled(false);
+
+        }else if(v == l){
+            String bogstav = l.getText().toString();
+            logik.gætBogstav(bogstav);
+            l.setEnabled(false);
+
+        }else if(v == m){
+            String bogstav = m.getText().toString();
+            logik.gætBogstav(bogstav);
+            m.setEnabled(false);
+
+        }else if(v == n){
+            String bogstav = n.getText().toString();
+            logik.gætBogstav(bogstav);
+            n.setEnabled(false);
+
+        }else if(v == o){
+            String bogstav = o.getText().toString();
+            logik.gætBogstav(bogstav);
+            o.setEnabled(false);
+
+        }else if(v == p){
+            String bogstav = p.getText().toString();
+            logik.gætBogstav(bogstav);
+            p.setEnabled(false);
+
+        }else if(v == q){
+            String bogstav = q.getText().toString();
+            logik.gætBogstav(bogstav);
+            q.setEnabled(false);
+
+        }else if(v == r){
+            String bogstav = r.getText().toString();
+            logik.gætBogstav(bogstav);
+            r.setEnabled(false);
+
+        }else if(v == s){
+            String bogstav = s.getText().toString();
+            logik.gætBogstav(bogstav);
+            s.setEnabled(false);
+
+        }else if(v == t){
+            String bogstav = t.getText().toString();
+            logik.gætBogstav(bogstav);
+            t.setEnabled(false);
+
+        }else if(v == u){
+            String bogstav = u.getText().toString();
+            logik.gætBogstav(bogstav);
+            u.setEnabled(false);
+
+        }else if(v == ve){
+            String bogstav = ve.getText().toString();
+            logik.gætBogstav(bogstav);
+            ve.setEnabled(false);
+
+        }else if(v == x){
+            String bogstav = x.getText().toString();
+            logik.gætBogstav(bogstav);
+            x.setEnabled(false);
+
+        }else if(v == y){
+            String bogstav = y.getText().toString();
+            logik.gætBogstav(bogstav);
+            y.setEnabled(false);
+
+        }else if(v == z){
+            String bogstav = z.getText().toString();
+            logik.gætBogstav(bogstav);
+            z.setEnabled(false);
+
+        }else if(v == ae){
+            String bogstav = ae.getText().toString();
+            logik.gætBogstav(bogstav);
+            ae.setEnabled(false);
+
+        }else if(v == oe){
+            String bogstav = oe.getText().toString();
+            logik.gætBogstav(bogstav);
+            oe.setEnabled(false);
+
+        }else if(v == aa){
+            String bogstav = aa.getText().toString();
+            logik.gætBogstav(bogstav);
+            aa.setEnabled(false);
+
+
 
         }else if(v == afslutKnap){
             visPauseskærm();
