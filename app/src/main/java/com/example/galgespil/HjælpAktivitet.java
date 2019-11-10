c4ed949c203d23a88d1a4a2f2fc5a1ef2699cdcc
Maria K <maria.kajgaard@gmail.com>
2019-11-10 22:00:13 +0100

    tilføjet scrollview i hjælp

--- app/src/main/java/com/example/galgespil/HjælpAktivitet.java
+++ app/src/main/java/com/example/galgespil/HjælpAktivitet.java
@@ -3,12 +3,18 @@ package com.example.galgespil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class HjælpAktivitet extends AppCompatActivity {
-
+TextView tekst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regler_aktivitet);
+
+
+        tekst = findViewById(R.id.forklaringTV);
+        tekst.setMovementMethod(new ScrollingMovementMethod());
     }
 }
